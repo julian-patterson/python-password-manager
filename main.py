aa9f175056f21056a69c2782a1b82e5213bc648b
julian-patterson <julianedwardpatterson@gmail.com>
2022-03-01 14:41:18 -0500

    fixed "change login info"

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while i >= 0:
     if authentication == password:
         while True:
 
-            services = input('What service would you like to access: \n 1)New Login \n 2)View Logins  \n 3) Change Login Information \n 4)Log Out  \n Please enter the number: ') 
+            services = input('What service would you like to access: \n 1)New Login \n 2)View Logins  \n 3)Change Login Information \n 4)Log Out  \n Please enter the number: ') 
 
 
             if services == '1':
@@ -31,9 +31,13 @@ while i >= 0:
                     new_password = (''.join(random.choice(letters+numbers+punctuation) for i in range (15)))
                     print('Your new password is: ' + new_password)
 
-                else:                   
+                if generate_password == 'N ':                   
                     new_password = input('Enter password: ')
-
+                
+                else:
+                    print('Going back...')
+                    sleep(1)
+                    break
 
                 new_entity = (str(new_website) + ', ' + str(new_username) + ', ' + str(new_password))
 
@@ -42,6 +46,8 @@ while i >= 0:
                     fw.write(str(new_entity))
                 print('Password Sucessfully Saved!')
                 sleep(1)
+
+
                 
             #add passwords that are available
             if services == '2':
@@ -55,6 +61,11 @@ while i >= 0:
                         requested_password = mylinelist[2]
                         print('Username: ' + requested_username + '\nPassword: ' + requested_password)
                         sleep(1)
+
+                    else:
+                        print('Account not found!')
+                        sleep(1)
+                        break
             
 
             if services == '3':
@@ -69,13 +80,19 @@ while i >= 0:
                         print('This is your current password: ' + requested_password)
                         change_password = input('Please enter your new password: ')
                         replaced_line = myline.replace(str(requested_password), str(change_password))
+                        with open('passwords.txt', 'w') as fw:
+                            del myline
                         print('Your new login information is: ' + '\n Username: ' + requested_username + '\n Password: ' + change_password)
                         with open ('passwords.txt', 'a') as fw:
-                            #replacing the line does not work
-                            fw.write(myline.replace(str(myline), str(replaced_line)))
+                            fw.write(str(replaced_line))
                             print('Your new password has been saved!')
                             sleep(1)
                             break
+                    
+                    else:
+                        print('Account not found!')
+                        sleep(1)
+                        break
 
             
 
